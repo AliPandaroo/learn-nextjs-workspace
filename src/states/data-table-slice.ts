810626435db3ba2022f44eb8029a071e7639c068
Ali Panddarou <atapndro21@gmail.com>
2025-01-30 15:11:44 +0330

    ADD data-table-slice state

---
+++ src/states/data-table-slice.ts
@@ -0,0 +1,157 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+type DataItem = {
+  [key: string]: any;
+};
+
+interface DataTableState {
+  /* PAGE */
+  currentPage: number;
+  pageSize: number;
+  totalPages: number;
+  totalItems: number;
+  // computed values
+  calculateTotalPages: (totalItems: number, pageSize: number) => number;
+
+  /* SORT */
+  sortBy: string;
+  sortDirection: "ASC" | "DESC";
+
+  /* FILTER */
+  filters: string[];
+
+  /* DATA */
+  columns: string[];
+  setColumns: (columns: string[]) => void;
+  fetchedData: DataItem;
+
+  /* ACTIONS */
+  setCurrentPage: (page: number) => void;
+  setPageSize: (size: number) => void;
+
+  setSortBy: (column: string) => void;
+  setSortDirection: (direction: "ASC" | "DESC") => void;
+  resetSort: () => void;
+
+  setFilter: (filter: string | string[]) => void;
+  resetFilters: () => void;
+
+  resetTable: () => void;
+
+  /* ASYNC ACTION */
+  fetchData: (url: string, supportsPagination?: boolean) => Promise<void>;
+}
+
+export const useDataTableState = create<DataTableState>()(
+  devtools((set) => ({
+    currentPage: 1,
+    pageSize: 10,
+    totalItems: 0,
+    totalPages: 0,
+
+    sortBy: "",
+    sortDirection: "ASC",
+
+    filters: [],
+
+    columns: [],
+    setColumns: (columns) => set((state: DataTableState) => ({ columns })),
+    fetchedData: (data: DataItem[]) => {
+      set((state: DataTableState) => ({ fetchedData: data }));
+    },
+
+    setCurrentPage: (page) => {
+      set((state: DataTableState) => ({ currentPage: page }));
+    },
+    setPageSize: (size) => set((state: DataTableState) => ({ pageSize: size })),
+    calculateTotalPages: (totalItems, pageSize) => {
+      return Math.ceil(totalItems / pageSize);
+    },
+
+    setSortBy: (column) => set((state: DataTableState) => ({ sortBy: column })),
+    setSortDirection: (direction) => {
+      set((state: DataTableState) => ({ sortDirection: direction }));
+    },
+    resetSort: () => {
+      set((state: DataTableState) => ({ sortBy: "", sortDirection: "ASC" }));
+    },
+
+    setFilter: (filter) => {
+      set((state: DataTableState) => {
+        let newFilters: string[];
+
+        if (Array.isArray(filter)) {
+          // filter:<[]>
+          // PUSH unique filters from the both arrays
+          newFilters = [...new Set([...state.filters, ...filter])];
+        } else {
+          // filter:<string>
+          if (state.filters.includes(filter)) {
+            // UNSET filter if it exists in filters[]
+            newFilters = state.filters.filter((f) => f !== filter);
+          } else {
+            // SET filter in filters[]
+            newFilters = [...state.filters, filter];
+          }
+        }
+
+        return { filters: newFilters };
+      });
+    },
+    resetFilters: () => {
+      set((state: DataTableState) => ({ filters: [] }));
+    },
+
+    resetTable: () => {
+      set({
+        currentPage: 1,
+        pageSize: 10,
+        totalItems: 0,
+        totalPages: 0,
+        sortBy: "",
+        sortDirection: "ASC",
+        filters: [],
+        columns: [],
+        fetchedData: [],
+      });
+    },
+
+    fetchData: async (url, supportsPagination = false) => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log({ data });
+
+        // get initial values from state
+        const { pageSize, currentPage, calculateTotalPages } =
+          useDataTableState.getState();
+
+        const items = data || []; // data
+        const meta = supportsPagination ? data.meta : {}; // metadata
+
+        const total_items = supportsPagination
+          ? meta.total_items
+          : items.length;
+        const current_page = supportsPagination
+          ? meta.current_page
+          : currentPage;
+        const page_size = supportsPagination ? meta.page_size : pageSize;
+        const total_page = supportsPagination
+          ? meta.total_pages
+          : calculateTotalPages(total_items, page_size);
+
+        // Update state
+        set({
+          fetchedData: items,
+          currentPage: current_page,
+          pageSize: page_size,
+          totalItems: total_items,
+          totalPages: total_page,
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    },
+  }))
+);
